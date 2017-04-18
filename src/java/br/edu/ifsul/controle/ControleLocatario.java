135a186a4836c33bbb8981f2437737b9ac544ff5
Huelison <huellisomkemerich2010@hotmail.com>
2017-04-18 13:16:18 -0300

    Commit incial

---
+++ src/java/br/edu/ifsul/controle/ControleLocatario.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifsul.controle;
+
+import br.edu.ifsul.dao.LocatarioDao;
+import br.edu.ifsul.modelo.Locatario;
+import br.edu.ifsul.util.Util;
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author Huelison
+ */
+@ManagedBean(name="controleLocatario")
+@SessionScoped
+public class ControleLocatario implements Serializable{
+    private LocatarioDao dao;
+    private Locatario objeto;
+
+    public ControleLocatario() {
+        dao = new LocatarioDao();
+    }
+
+    public String listar(){
+        return "/privado/locatario/listar?faces-redirect=true";
+    }
+    
+    public String novo(){
+        objeto = new Locatario();
+        
+        return "formulario";
+    }
+    
+    public String salvar(){
+        if(dao.salvar(objeto)){
+            Util.mensagemInformacao(dao.getMensagem());
+            return "listar";
+        }else{
+            Util.mensagemErro(dao.getMensagem());
+            return "formulario";
+        }
+    }
+    
+    public String cancelar(){
+        return "listar";
+    }
+    
+    public String editar(Integer id){
+        try{
+            objeto = dao.localizar(id);
+            return "formulario";
+        }catch(Exception e){
+            Util.mensagemErro("Erro ao recuperar objeto"+Util.getMensagemErro(e));
+            return "listar";
+        }
+    }
+    
+    public void remover(Integer id){
+        objeto = dao.localizar(id);
+        if(dao.remover(objeto)){
+            Util.mensagemInformacao(dao.getMensagem());
+        }else{
+            Util.mensagemErro(dao.getMensagem());
+        }
+            
+    }
+    
+    public LocatarioDao getDao() {
+        return dao;
+    }
+
+    public void setDao(LocatarioDao dao) {
+        this.dao = dao;
+    }
+
+    public Locatario getObjeto() {
+        return objeto;
+    }
+
+    public void setObjeto(Locatario objeto) {
+        this.objeto = objeto;
+    }
+    
+    
+}
